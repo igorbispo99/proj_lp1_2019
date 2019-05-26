5892d3c586184a25d4dbdbf8630f8625812cf8f3
HSuper-8 <hevelyn.sthefany@gmail.com>
2019-05-26 12:50:13 -0300

    buildTree

--- tree.go
+++ tree.go
@@ -1,7 +1,7 @@
 package forest
 
-import( "fmt"
-		"math"
+import( //"fmt"
+		//"math"
 		"math/rand"
 		//classifier
 
@@ -35,13 +35,61 @@ func newTree(inputs [][]interface{}, labels []int, nSamples int, nFeatures int) 
 	return tree
 }
 
-//func buildTree(samples [][]interface{}, samplesLabels []int, nFeatures int) *Node/{	
-//}
+func buildTree(samples [][]interface{}, samplesLabels []int, nFeatures int) *Node{
+	nColumns := len(samples[0]) 	// amostras
+	nRows := nFeatures				// características
+	selectedColumns := 	randomColumns(nColumns, nFeatures)			
+
+	bestGain := 0.0
+	bestPartL := make([]int, 0, len(samples))
+	bestPartR := make([]int, 0, len(samples))
+	bestTotalL := 0 
+	bestTotalR := 0
+	var bestThreshold interface{}
+	var bestCol int
+	var best_column_type string
+
+	total := len(samplesLabels)
+	pMap := make(map[int]float64)   // pMap : probability map
+	for i:=0; i<total; i++{  
+		pMap[samplesLabels[i]] += 1	
+    }
+
+	//entropy := Entropy(pMap, total) // Criterio de impureza escolhido. Testar com Gini
+
+	for _,col := range selectedColumns{
+		// encontra melhor ponto de corte dado a região atual do espaço de acordo com critério de impureza escolhido
+		gain, threshold, totalL, totalR := cutPoint(samples, col, samplesLabels, entropy)
+
+		if gain >= bestGain{
+			bestGain = gain
+			bestThreshold = threshold
+			bestCol = col
+			bestTotalL = totalL
+			bestTotalR = totalR
+		}
+	}
+
+	if bestGain > 0 && bestTotalL > 0 && bestTotalR > 0 {  
+		node := &Node{}
+		node.value = bestThreshold
+		node.columnNumber = bestCol
+		// aplicando o limiar para particionar o espaço:
+		//splitSamples(samples, bestCol, bestThreshold, &bestPartL, &bestPartR)
+
+		// contruindo árvore recursivamente para os sub-espaço da direita e da esquerda.
+		//node.left = buildTree(getSamples(samples, bestPartL), getLabels(samplesLabels, bestPartL),  nFeatures)
+		//node.right = buildTree(getSamples(samples, bestPartR), getLabels(samplesLabels, bestPartR),  nFeatures)
+		return node
+	}
+	
+	return leafNode(samplesLabels)
+}
 
 // Node Struct _________________________________________________________
 type Node struct{
 	value interface{}
-	labels []int
+	labels map[int]int
 	//decision int
 	//threshold int
 	columnNumber int 
@@ -49,3 +97,13 @@ type Node struct{
 	right *Node
 }
 
+func leafNode(labels []int) *Node{
+	counter := make(map[int]int)
+	for _,label := range labels{
+		counter[label] += 1
+	}
+
+	node := &Node{}
+	node.labels = counter
+	return node
+}
