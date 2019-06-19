22b37df03bb975394d7a1a7284351aaebd88a791
HSuper-8 <hevelyn.sthefany@gmail.com>
2019-06-19 20:42:59 -0300

    modulo tree comentado. Partição das amostras em paralelo. Add maxDepth. Nao testado. AJEITAR ensemble!

--- Forest/tree.go
+++ Forest/tree.go
@@ -3,99 +3,110 @@ package Forest
 import ( //"fmt"
 	"math"
 	"math/rand"
-	//classifier
+	"sync"
 )
 
 // Tree Struct ____________________________________________________________
 type Tree struct {
 	root *Node
-
-	features  []int
-	nFeatures int
-
-	maxDepth int
-	nNodes   int
-	nClasses int
+	depth int
 }
 
-func NewTree(inputs [][]interface{}, labels []int, nSamples int, nFeatures int) *Tree {
+// Retorna uma nova árvore com nSamples (número de amostras) aleatórias do input.
+func NewTree(input [][]interface{}, labels []int, nSamples int, nSelectedFeatures int, maxDepth int) *Tree {
 	samplesLabels := make([]int, nSamples)
 	samples := make([][]interface{}, nSamples)
 
-	// TODO Talvez paralelizar isso seja bom, checar resultados
+	wg := sync.WaitGroup{}
 
 	for i := 0; i < nSamples; i++ {
-		j := int(rand.Float64() * float64(len(inputs))) // retorna o inteiro do float aleatório gerado entre [0.0, 1.0] * len(inputs) ou seja entre [0.0, len(inputs)]
-		samples[i] = inputs[j]
-		samplesLabels[i] = labels[j]
+		wg.Add(1)
+		go func (i int){
+			j := int(rand.Float64() * float64(len(input))) // retorna o inteiro do float aleatório gerado entre [0.0, 1.0] * len(input) ou seja entre [0.0, len(input)]
+			samples[i] = input[j]
+			samplesLabels[i] = labels[j]
+			wg.Done() // decrementa o contador WaitGroup por um.
+		}(i)
 	}
-
+	wg.Wait() // Aguarde até que o contador WaitGroup seja zero. Espera todas as goroutines terminarem
 	tree := &Tree{}
-	tree.root = buildTree(samples, samplesLabels, nFeatures)
+	tree.depth = 1;
+	tree.root = buildTree(samples, samplesLabels, nSelectedFeatures, maxDepth, tree)
 
 	return tree
 }
 
-func buildTree(samples [][]interface{}, samplesLabels []int, nFeatures int) *Node {
-	nColumns := len(samples[1])                           // Features
-	selectedColumns := randomColumns(nColumns, nFeatures) // feat selecionadas para o cutPoint
+// Cria a árvore recursivamente
+// - Seleciona algumas Características, - Calcula a Entropia, - encontra melhor ponto de corte (Característica de Corte), 
+// - Separa os Rótulos, - Calcula sub-árvores à esquerda e à direita
+func buildTree(samples [][]interface{}, samplesLabels []int, nSelectedFeatures int, maxDepth int, tree *Tree) *Node {
+	
+	if(tree.depth != maxDepth){
+		tree.depth += 1;
+		nFeatures := len(samples[1])                           // Features
+		selectedFeatures := randomFeatures(nFeatures, nSelectedFeatures) // características selecionadas para o cutPoint
 
-	bestGain := 0.0
-	bestPartL := make([]int, 0, len(samples))
-	bestPartR := make([]int, 0, len(samples))
-	bestTotalL := 0
-	bestTotalR := 0
-	var bestThreshold interface{}
-	var bestCol int
+		bestGain := 0.0
+		bestPartL := make([]int, 0, len(samples))
+		bestPartR := make([]int, 0, len(samples))
+		bestTotalL := 0
+		bestTotalR := 0
+		var bestThreshold interface{}
+		var bestThresholdIdx int
 
-	total := len(samplesLabels)
-	pMap := make(map[int]float64) // pMap : probability map
-	for i := 0; i < total; i++ {
-		pMap[samplesLabels[i]] += 1
-	}
-
-	entropy := Entropy(pMap, total) // Criterio de impureza escolhido. Testar com Gini
-
-	for _, col := range selectedColumns {
-		// encontra melhor ponto de corte dado a região atual do espaço de acordo com critério de impureza escolhido
-		gain, threshold, totalL, totalR := cutPoint(samples, col, samplesLabels, entropy)
-
-		if gain >= bestGain {
-			bestGain = gain
-			bestThreshold = threshold
-			bestCol = col
-			bestTotalL = totalL
-			bestTotalR = totalR
+		total := len(samplesLabels)
+		qtdMap := make(map[int]float64) // qtdMap : mapa das quatidades de amostras por Rótulo
+		for i := 0; i < total; i++ {
+			qtdMap[samplesLabels[i]] += 1
 		}
-	}
 
-	if bestGain > 0 && bestTotalL > 0 && bestTotalR > 0 {
-		node := &Node{}
-		node.value = bestThreshold
-		node.columnNumber = bestCol
-		// aplicando o limiar para particionar o espaço:
-		splitSamples(samples, bestCol, bestThreshold, &bestPartL, &bestPartR)
+		entropy := Entropy(qtdMap, total) // Criterio de impureza escolhido. Testar com Gini
 
-		// contruindo árvore recursivamente para os sub-espaço da direita e da esquerda.
-		node.left = buildTree(getSamples(samples, bestPartL), getLabels(samplesLabels, bestPartL), nFeatures)
-		node.right = buildTree(getSamples(samples, bestPartR), getLabels(samplesLabels, bestPartR), nFeatures)
-		return node
+		// encontra melhor ponto de corte dado a região atual do espaço de acordo com critério de impureza escolhido
+		for _, col := range selectedFeatures {
+			gain, threshold, totalL, totalR := cutPoint(samples, col, samplesLabels, entropy)
+
+			if gain >= bestGain {
+				bestGain = gain
+				bestThreshold = threshold
+				bestThresholdIdx = col
+				bestTotalL = totalL
+				bestTotalR = totalR
+			}
+		}
+
+		if bestGain > 0 && bestTotalL > 0 && bestTotalR > 0 {
+			node := &Node{}
+			node.threshold = bestThreshold
+			node.thredholdIdx = bestThresholdIdx
+			// aplicando o limiar para particionar o espaço:
+			splitSpace(samples, bestThresholdIdx, bestThreshold, &bestPartL, &bestPartR)
+
+			// contruindo árvore recursivamente para os sub-espaço da direita e da esquerda.
+			
+			node.left = buildTree(getSamples(samples, bestPartL), getLabels(samplesLabels, bestPartL), nFeatures, maxDepth, tree)
+			node.right = buildTree(getSamples(samples, bestPartR), getLabels(samplesLabels, bestPartR), nFeatures, maxDepth, tree)
+			return node
+		} else {
+			return leafNode(samplesLabels)
+		}
 	}
 
 	return leafNode(samplesLabels)
 }
 
-func randomColumns(nColumns int, nFeatures int) []int {
-	tmp := make([]int, nColumns)
-	for i := 0; i < nColumns; i++ {
+// Retorna uma vetor com nSelectedFeatures índices das Características. Para isso, primeiro embaralha as Características.
+func randomFeatures(nFeatures int, nSelectedFeatures int) []int {
+	tmp := make([]int, nFeatures)
+	for i := 0; i < nFeatures; i++ {
 		tmp[i] = i
 	}
-	for i := 0; i < nFeatures; i++ {
-		j := i + int(rand.Float64()*float64(nColumns-i))
+	for i := 0; i < nSelectedFeatures; i++ {
+		j := i + int(rand.Float64()*float64(nFeatures-i)) // j: número aleatório entre 0 e nFeatures
 		tmp[i], tmp[j] = tmp[j], tmp[i]
 	}
 
-	return tmp[:nFeatures]
+	return tmp[:nSelectedFeatures]
 }
 
 func Entropy(p_vec map[int]float64, total int) float64 {
@@ -132,6 +143,9 @@ func Gini(p_vec map[string]float64) float64 { // VERIFICAR
 	return impure
 }
 
+/* Cada divisão do espaço é representada por um nó na árvore de decisão. A primeira divisão (nós raiz da árvore) leva em consideração todos os exemplos (separados aleatóriamente) do espaço ao encontrar o ponto de corte que maximiza a pureza, de acordo com algum critério de impureza, das sub-regiões resultantes. 
+Para encontrar melhor ponto de corte, são testados todos os possíveis, ou seja, para cada atributo e valores possíveis calcula-se o ganho de informação (quão pura a divisão torna o espaço) para cada um dos pontos de corte candidatos. Após essa etapa, é escolhido o candidato com maior ganho de informação para ser o ponto de corte do nó em questão.
+*/
 func cutPoint(samples [][]interface{}, c int, samplesLabels []int, currentEntropy float64) (float64, interface{}, int, int) {
 	var bestThreshold interface{}
 	bestGain := 0.0
@@ -175,7 +189,8 @@ func cutPoint(samples [][]interface{}, c int, samplesLabels []int, currentEntrop
 	return bestGain, bestThreshold, totalL, totalR
 }
 
-func splitSamples(samples [][]interface{}, c int, value interface{}, partL *[]int, partR *[]int) {
+// Cria os sub-espaço da direita e da esquerda (separa o espaço das amostras). partL e partR são vetores de indices
+func splitSpace(samples [][]interface{}, c int, value interface{}, partL *[]int, partR *[]int) {
 	for j := 0; j < len(samples); j++ {
 		if samples[j][c].(float64) <= value.(float64) {
 			*partL = append(*partL, j)
@@ -187,6 +202,7 @@ func splitSamples(samples [][]interface{}, c int, value interface{}, partL *[]in
 
 }
 
+// Função auxiliar para pegar as amotras de um dado sub-espaço
 func getSamples(samples [][]interface{}, idxPart []int) [][]interface{} {
 	samplesPart := make([][]interface{}, len(idxPart))
 	for i := 0; i < len(idxPart); i++ {
@@ -195,6 +211,7 @@ func getSamples(samples [][]interface{}, idxPart []int) [][]interface{} {
 	return samplesPart
 }
 
+// Função auxiliar para pegar os Rótulos de um dado sub-espaço
 func getLabels(samples []int, idxPart []int) []int {
 	labelsPart := make([]int, len(idxPart))
 	for i := 0; i < len(idxPart); i++ {
@@ -205,17 +222,16 @@ func getLabels(samples []int, idxPart []int) []int {
 
 // Node Struct _________________________________________________________
 type Node struct {
-	value  interface{}
-	labels map[int]int
-	//decision int
-	//threshold int
-	columnNumber int
+	threshold  interface{}
+	thredholdIdx int
+	labels map[int]int 			// quantidade de exemplos por classe nesse nó folha
 	left         *Node
 	right        *Node
 }
 
+// Retorna Nó folha
 func leafNode(labels []int) *Node {
-	counter := make(map[int]int)
+	counter := make(map[int]int) //quantidade de exemplos por classe nesse nó folha
 	for _, label := range labels {
 		counter[label] += 1
 	}
